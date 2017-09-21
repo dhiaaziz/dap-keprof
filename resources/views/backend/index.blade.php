64140f875e38392a77c09ca8689aa765da9f4970
Dhia Aziz <azizwkwkwk@gmail.com>
2017-09-21 21:04:35 +0700

    Initial commit

---
+++ resources/views/backend/index.blade.php
@@ -0,0 +1,15 @@
+@extends('backend.layouts.master')
+
+@section('title')
+   TEST
+@endsection
+
+@section('content')
+<section class="content">
+    <div class="container-fluid">
+        <div class="block-header">
+            <h2>BLANK PAGE</h2>
+        </div>
+    </div>
+</section>
+@endsection
---
+++ resources/views/backend/layouts/master.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title> @yield('title') </title>
+
+    @include('backend.layouts.header')
+    @yield('more-style')
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+</head>
+
+<body class="theme-red">
+    <!-- Page Loader -->
+    <div class="page-loader-wrapper">
+        <div class="loader">
+            <div class="preloader">
+                <div class="spinner-layer pl-red">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
+                </div>
+            </div>
+            <p>Please wait...</p>
+        </div>
+    </div>
+    <!-- #END# Page Loader -->
+    <!-- Overlay For Sidebars -->
+    <div class="overlay"></div>
+    <!-- #END# Overlay For Sidebars -->
+    <!-- Search Bar -->
+    <div class="search-bar">
+        <div class="search-icon">
+            <i class="material-icons">search</i>
+        </div>
+        <input type="text" placeholder="START TYPING...">
+        <div class="close-search">
+            <i class="material-icons">close</i>
+        </div>
+    </div>
+    <!-- #END# Search Bar -->
+    <!-- Top Bar -->
+    @include('backend.layouts.navtop')
+    <!-- #Top Bar -->
+    <!--NavBar side-->
+    @include('backend.layouts.navbar')
+    <!--#NavBar side-->
+    <!--Content-->
+    @yield('content')
+    <!--#Content-->
+
+    <!--Footer-->
+    @include('backend.layouts.footer')
+    @yield('more-script')
+    <!--#Footer-->
+</body>
+
+</html>
